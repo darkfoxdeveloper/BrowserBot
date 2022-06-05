91fbff78c1db85228426a55b7d02219af95e36e6
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2022-06-05 23:18:09 +0200

    All working with options for show or hide dev tools window

--- BrowserBot/Main.Designer.cs
+++ BrowserBot/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnStart = new MetroSet_UI.Controls.MetroSetButton();
             this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
             this.tbxScripts = new System.Windows.Forms.TextBox();
@@ -35,6 +36,11 @@
             this.lblLoadURL = new MetroSet_UI.Controls.MetroSetLabel();
             this.cbxProfiles = new MetroSet_UI.Controls.MetroSetComboBox();
             this.lblProfiles = new MetroSet_UI.Controls.MetroSetLabel();
+            this.btnSaveProfile = new MetroSet_UI.Controls.MetroSetButton();
+            this.loadingPicture = new System.Windows.Forms.PictureBox();
+            this.switchEnableDevTools = new MetroSet_UI.Controls.MetroSetSwitch();
+            this.lblEnableDevTools = new MetroSet_UI.Controls.MetroSetLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
@@ -47,7 +53,7 @@
             this.btnStart.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
             this.btnStart.HoverTextColor = System.Drawing.Color.White;
             this.btnStart.IsDerivedStyle = true;
-            this.btnStart.Location = new System.Drawing.Point(15, 892);
+            this.btnStart.Location = new System.Drawing.Point(6, 892);
             this.btnStart.Name = "btnStart";
             this.btnStart.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
             this.btnStart.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
@@ -55,7 +61,7 @@
             this.btnStart.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
             this.btnStart.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
             this.btnStart.PressTextColor = System.Drawing.Color.White;
-            this.btnStart.Size = new System.Drawing.Size(388, 72);
+            this.btnStart.Size = new System.Drawing.Size(206, 72);
             this.btnStart.Style = MetroSet_UI.Enums.Style.Light;
             this.btnStart.StyleManager = null;
             this.btnStart.TabIndex = 0;
@@ -67,9 +73,9 @@
             // chromiumWebBrowser1
             // 
             this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
-            this.chromiumWebBrowser1.Location = new System.Drawing.Point(1, 6);
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(6, 96);
             this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
-            this.chromiumWebBrowser1.Size = new System.Drawing.Size(997, 374);
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(992, 284);
             this.chromiumWebBrowser1.TabIndex = 2;
             this.chromiumWebBrowser1.Text = "chromiumWebBrowser1";
             // 
@@ -120,7 +126,7 @@
             this.cbxProfiles.FormattingEnabled = true;
             this.cbxProfiles.IsDerivedStyle = true;
             this.cbxProfiles.ItemHeight = 40;
-            this.cbxProfiles.Location = new System.Drawing.Point(669, 914);
+            this.cbxProfiles.Location = new System.Drawing.Point(665, 906);
             this.cbxProfiles.Name = "cbxProfiles";
             this.cbxProfiles.SelectedItemBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
             this.cbxProfiles.SelectedItemForeColor = System.Drawing.Color.White;
@@ -136,7 +142,7 @@
             // 
             this.lblProfiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblProfiles.IsDerivedStyle = true;
-            this.lblProfiles.Location = new System.Drawing.Point(560, 923);
+            this.lblProfiles.Location = new System.Drawing.Point(556, 916);
             this.lblProfiles.Name = "lblProfiles";
             this.lblProfiles.Size = new System.Drawing.Size(103, 26);
             this.lblProfiles.Style = MetroSet_UI.Enums.Style.Light;
@@ -146,11 +152,92 @@
             this.lblProfiles.ThemeAuthor = "Narwin";
             this.lblProfiles.ThemeName = "MetroLite";
             // 
+            // btnSaveProfile
+            // 
+            this.btnSaveProfile.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnSaveProfile.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnSaveProfile.DisabledForeColor = System.Drawing.Color.Gray;
+            this.btnSaveProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSaveProfile.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.btnSaveProfile.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.btnSaveProfile.HoverTextColor = System.Drawing.Color.White;
+            this.btnSaveProfile.IsDerivedStyle = true;
+            this.btnSaveProfile.Location = new System.Drawing.Point(218, 892);
+            this.btnSaveProfile.Name = "btnSaveProfile";
+            this.btnSaveProfile.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnSaveProfile.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnSaveProfile.NormalTextColor = System.Drawing.Color.White;
+            this.btnSaveProfile.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.btnSaveProfile.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.btnSaveProfile.PressTextColor = System.Drawing.Color.White;
+            this.btnSaveProfile.Size = new System.Drawing.Size(206, 72);
+            this.btnSaveProfile.Style = MetroSet_UI.Enums.Style.Light;
+            this.btnSaveProfile.StyleManager = null;
+            this.btnSaveProfile.TabIndex = 8;
+            this.btnSaveProfile.Text = "SAVE TO PROFILE";
+            this.btnSaveProfile.ThemeAuthor = "Narwin";
+            this.btnSaveProfile.ThemeName = "MetroLite";
+            this.btnSaveProfile.Click += new System.EventHandler(this.BtnSaveProfile_Click);
+            // 
+            // loadingPicture
+            // 
+            this.loadingPicture.BackColor = System.Drawing.Color.Transparent;
+            this.loadingPicture.Image = global::BrowserBot.Properties.Resources.boticon;
+            this.loadingPicture.InitialImage = global::BrowserBot.Properties.Resources.boticon;
+            this.loadingPicture.Location = new System.Drawing.Point(6, 96);
+            this.loadingPicture.Name = "loadingPicture";
+            this.loadingPicture.Size = new System.Drawing.Size(992, 284);
+            this.loadingPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.loadingPicture.TabIndex = 9;
+            this.loadingPicture.TabStop = false;
+            // 
+            // switchEnableDevTools
+            // 
+            this.switchEnableDevTools.BackColor = System.Drawing.Color.Transparent;
+            this.switchEnableDevTools.BackgroundColor = System.Drawing.Color.Empty;
+            this.switchEnableDevTools.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(159)))), ((int)(((byte)(147)))));
+            this.switchEnableDevTools.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.switchEnableDevTools.CheckState = MetroSet_UI.Enums.CheckState.Unchecked;
+            this.switchEnableDevTools.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(205)))), ((int)(((byte)(205)))));
+            this.switchEnableDevTools.DisabledCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.switchEnableDevTools.DisabledUnCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(205)))), ((int)(((byte)(205)))), ((int)(((byte)(205)))));
+            this.switchEnableDevTools.IsDerivedStyle = true;
+            this.switchEnableDevTools.Location = new System.Drawing.Point(15, 986);
+            this.switchEnableDevTools.Name = "switchEnableDevTools";
+            this.switchEnableDevTools.Size = new System.Drawing.Size(58, 22);
+            this.switchEnableDevTools.Style = MetroSet_UI.Enums.Style.Light;
+            this.switchEnableDevTools.StyleManager = null;
+            this.switchEnableDevTools.Switched = false;
+            this.switchEnableDevTools.SymbolColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(92)))), ((int)(((byte)(92)))));
+            this.switchEnableDevTools.TabIndex = 10;
+            this.switchEnableDevTools.Text = "Enable Dev Tools";
+            this.switchEnableDevTools.ThemeAuthor = "Narwin";
+            this.switchEnableDevTools.ThemeName = "MetroLite";
+            this.switchEnableDevTools.UnCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            // 
+            // lblEnableDevTools
+            // 
+            this.lblEnableDevTools.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblEnableDevTools.IsDerivedStyle = true;
+            this.lblEnableDevTools.Location = new System.Drawing.Point(89, 982);
+            this.lblEnableDevTools.Name = "lblEnableDevTools";
+            this.lblEnableDevTools.Size = new System.Drawing.Size(178, 26);
+            this.lblEnableDevTools.Style = MetroSet_UI.Enums.Style.Light;
+            this.lblEnableDevTools.StyleManager = null;
+            this.lblEnableDevTools.TabIndex = 11;
+            this.lblEnableDevTools.Text = "Enable Dev Tools";
+            this.lblEnableDevTools.ThemeAuthor = "Narwin";
+            this.lblEnableDevTools.ThemeName = "MetroLite";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1009, 975);
+            this.ClientSize = new System.Drawing.Size(1009, 1021);
+            this.Controls.Add(this.lblEnableDevTools);
+            this.Controls.Add(this.switchEnableDevTools);
+            this.Controls.Add(this.loadingPicture);
+            this.Controls.Add(this.btnSaveProfile);
             this.Controls.Add(this.lblProfiles);
             this.Controls.Add(this.cbxProfiles);
             this.Controls.Add(this.lblLoadURL);
@@ -158,9 +245,11 @@
             this.Controls.Add(this.tbxScripts);
             this.Controls.Add(this.chromiumWebBrowser1);
             this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "BrowserBot";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loadingPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +264,9 @@
         private MetroSet_UI.Controls.MetroSetLabel lblLoadURL;
         private MetroSet_UI.Controls.MetroSetComboBox cbxProfiles;
         private MetroSet_UI.Controls.MetroSetLabel lblProfiles;
+        private MetroSet_UI.Controls.MetroSetButton btnSaveProfile;
+        private PictureBox loadingPicture;
+        private MetroSet_UI.Controls.MetroSetSwitch switchEnableDevTools;
+        private MetroSet_UI.Controls.MetroSetLabel lblEnableDevTools;
     }
 }
--- BrowserBot/Main.cs
+++ BrowserBot/Main.cs
@@ -1,4 +1,5 @@
 using CefSharp;
+using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 
 namespace BrowserBot
@@ -19,37 +20,36 @@ namespace BrowserBot
             {
                 Directory.CreateDirectory(SaveProfilesPath);
             }
-            // SET demo profile
-            Profile p = new() { Name = "DEMO1", ScriptContents = @"
-const linkUrl = $('#link').attr('href');
-function copyToClipBoard() {
-    var textArea = document.createElement('textarea');
-  textArea.value = linkUrl;
+            /*Profile p = new() { Name = "DEMO1", ScriptContents = @"
+                const linkUrl = $('#link').attr('href');
+                function copyToClipBoard() {
+                    var textArea = document.createElement('textarea');
+                  textArea.value = linkUrl;
   
-  // Avoid scrolling to bottom
-  textArea.style.top = '0';
-  textArea.style.left = '0';
-  textArea.style.position = 'fixed';
+                  // Avoid scrolling to bottom
+                  textArea.style.top = '0';
+                  textArea.style.left = '0';
+                  textArea.style.position = 'fixed';
 
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+                  document.body.appendChild(textArea);
+                  textArea.focus();
+                  textArea.select();
 
-  try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
-  }
+                  try {
+                    var successful = document.execCommand('copy');
+                    var msg = successful ? 'successful' : 'unsuccessful';
+                    console.log('Fallback: Copying text command was ' + msg);
+                  } catch (err) {
+                    console.error('Fallback: Oops, unable to copy', err);
+                  }
 
-  document.body.removeChild(textArea);
-    alert('Script Loaded');
-}
-copyToClipBoard();
-", Url = "https://grantorrent.co/torrent/oal5k9wkoy/" };
+                  document.body.removeChild(textArea);
+                    alert('Script Loaded');
+                }
+                copyToClipBoard();
+            ", Url = "https://grantorrent.co/torrent/oal5k9wkoy/" };
             File.WriteAllText(Path.Combine(SaveProfilesPath, "ProfileDemo1.json"), JsonConvert.SerializeObject(p));
-            //
+            */
             foreach(string Path in Directory.GetFiles(SaveProfilesPath, "*.json"))
             {
                 Profile pTemp = JsonConvert.DeserializeObject<Profile>(File.ReadAllText(Path));
@@ -68,7 +68,11 @@ copyToClipBoard();
         private void BtnLoad_Click(object sender, EventArgs e)
         {
             chromiumWebBrowser1.LoadUrl(tbxUrl.Text);
-            //chromiumWebBrowser1.ShowDevTools();
+            loadingPicture.Visible = false;
+            if (switchEnableDevTools.CheckState == MetroSet_UI.Enums.CheckState.Checked)
+            {
+                chromiumWebBrowser1.ShowDevTools();
+            }
         }
 
         private void CbxProfiles_SelectedIndexChanged(object sender, EventArgs e)
@@ -83,6 +87,13 @@ copyToClipBoard();
                     tbxScripts.Text = pTemp.ScriptContents;
                 }
             }
+        }
+
+        private void BtnSaveProfile_Click(object sender, EventArgs e)
+        {
+            string name = Interaction.InputBox("Input the name of the Profile", "Save Profile", "Profile-"+DateTime.Now.Ticks.ToString());
+            Profile p = new() { Name = name, ScriptContents = tbxScripts.Text, Url = tbxUrl.Text };
+            File.WriteAllText(Path.Combine(SaveProfilesPath, $"Profile_{Name}.json"), JsonConvert.SerializeObject(p));
         }
     }
 }
